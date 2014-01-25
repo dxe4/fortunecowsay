64298aabb264caa3a6ca19b355badd3cc00597fe
charalambosp <papaloizouc@gmail.com>
2014-01-25 23:25:05 +0000

    fix nginx

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,28 +2,4 @@ apt-get install python-virtualenv nginx
 virtualenv -p /usr/bin/python3 venv
 . ./venv/bin/activate
 pip install -r requirements.txt
-echo "
-upstream app_server_djangoapp {
-    server localhost:5000 fail_timeout=0;
-}
-server {
-    #EC2 instance security group must be configured to accept http connections over Port 80
-    listen 80;
-    server_name $1
-    access_log  /var/log/nginx/guni-access.log;
-    error_log  /var/log/nginx/guni-error.log info;
-
-    keepalive_timeout 5;
-
-    location / {
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $http_host;
-        proxy_redirect off;
-
-        if (!-f $request_filename) {
-            break;
-        }
-    }
-}
-
-" > /etc/nginx/sites-enabled/default
+cp default /etc/nginx/sites-enabled/default
